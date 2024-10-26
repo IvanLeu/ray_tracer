854970d1710144809b5598f768a94b5eb1a41b85
ivanleu14@gmail.com <ivanleu14@gmail.com>
2024-10-26 23:29:22 +0300

    winmain

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include "Application.h"
 
-int main() {
-	
-	return Application{}.Run();
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd) 
+{
+	try {
+		return Application{}.Run();
+	}
+	catch (...) {
+		MessageBox(nullptr, "No details available", "Unknown Exception", MB_OK | MB_ICONEXCLAMATION | MB_TASKMODAL);
+	}
 
+	return -1;
 }
